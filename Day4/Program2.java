3a88f9f623bcc6a42abd01fe94953a0ec70afe74
Varshitha <50805446+Varshitha019@users.noreply.github.com>
2020-05-21 17:34:31 +0530

    Create Program2.java

---
+++ Day4/Program2.java
@@ -0,0 +1,25 @@
+2.write a simple applet java program to check whether the given number is armstrong number or not
+import java.util.Scanner;
+public class Main {
+	
+    public static void main(String[] args) {
+
+        int num, number, temp, total = 0;
+        System.out.println("Ënter 3 Digit Number");
+        Scanner scanner = new Scanner(System.in);
+        num = scanner.nextInt();
+        scanner.close();
+        number = num;
+
+        for( ;number!=0;number /= 10)
+        {
+            temp = number % 10;
+            total = total + temp*temp*temp;
+        }
+
+        if(total == num)
+            System.out.println(num + " is an Armstrong number");
+        else
+            System.out.println(num + " is not an Armstrong number");
+    }
+}
