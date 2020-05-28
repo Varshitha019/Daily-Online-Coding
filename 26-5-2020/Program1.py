e5c24bd7370e28407a421de22d3c93658fe86219
Varshitha <50805446+Varshitha019@users.noreply.github.com>
2020-05-28 14:43:28 +0530

    Create Program1.py

---
+++ 26-5-2020/Program1.py
@@ -0,0 +1,6 @@
+1.Python Program to Print all Integers that Aren’t Divisible by Either 2 or 3 and Lie between 1 and 50
+
+print ('Hello World')
+for i in range(1,51):
+	if(i%2!=0 and i%3!=0):
+		print(i)
