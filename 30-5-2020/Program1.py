9bdbaab096d06c58f38e7079ee062acaa2c4acd0
Varshitha <50805446+Varshitha019@users.noreply.github.com>
2020-05-30 19:43:39 +0530

    Create Program1.py

---
+++ 30-5-2020/Program1.py
@@ -0,0 +1,11 @@
+1. Python program to read a number and print the pattern
+Enter number of rows: 5
+
+**
+*
+
+rows = int(input("Enter the number of rows "))
+for i in range(rows + 1, 0, -1):
+    for j in range(0, i - 1):
+        print("*", end=' ')
+    print(" ")
