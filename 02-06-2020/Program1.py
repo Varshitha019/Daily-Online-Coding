f14757064cdba4c67d94f4e804135c45d54f0323
Varshitha <50805446+Varshitha019@users.noreply.github.com>
2020-06-02 14:58:42 +0530

    Create Program1.py

---
+++ 02-06-2020/Program1.py
@@ -0,0 +1,8 @@
+1. Python program to return a list containing first and last element using list slicing method.
+
+
+test_list = [ 2,4,5,6,7] 
+print ("The list is : " +  str(test_list)) 
+
+res = [ test_list[0], test_list[-1] ]  
+print ("List with first and last element: " +  str(res)) 
