7c248a8452bd836747097f9c661f84447ee9ccfc
Varshitha <50805446+Varshitha019@users.noreply.github.com>
2020-06-03 18:56:43 +0530

    Create Program3.py

---
+++ 03-06-2020/Program3.py
@@ -0,0 +1,18 @@
+1.Write a python program for the given problem statement.
+Problem statement:
+Take a list of length 3 containing integers, find out which is larger, first or last one and set all
+the elements in the list to be that value. Print the updated list
+eg:
+1)Input - Given list: [1, 2, 3]
+Output- [3,3,3]
+2)Input - Given list: [2, 11, 3]
+Output- [3,3,3]
+
+
+
+lst=[]
+n = int(input("Enter number of elements : "))
+for i in range(0, n):
+    ele = int(input())
+    lst.append(3)
+print(lst)
