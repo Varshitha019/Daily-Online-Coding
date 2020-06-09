de715e84212f2950af20d3482c18edeb89be996f
Varshitha <50805446+Varshitha019@users.noreply.github.com>
2020-06-09 19:00:22 +0530

    Create Program3.java

---
+++ 09-06-2020/Program3.java
@@ -0,0 +1,17 @@
+3. Write a Java Program to remove all white spaces from a string without using replace()
+
+
+public class Main {  
+    public static void main(String[] args) {  
+        String str = "My name is Varshitha ";  
+        char[] strArray = str.toCharArray();  
+        StringBuffer stringBuffer = new StringBuffer();  
+        for (int i = 0; i < strArray.length; i++) {  
+            if ((strArray[i] != ' ') && (strArray[i] != '\t')) {  
+                stringBuffer.append(strArray[i]);  
+            }  
+        }  
+        String noSpaceStr2 = stringBuffer.toString();  
+        System.out.println(noSpaceStr2);  
+    }  
+}
