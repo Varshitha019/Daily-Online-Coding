f724ef91a28d42a35c7bd6d1e7c4178cfcc5e7e5
Varshitha <50805446+Varshitha019@users.noreply.github.com>
2020-06-26 12:13:27 +0530

    Create Program2.java

---
+++ 25-06-2020/Program2.java
@@ -0,0 +1,11 @@
+2. Write a java program to find Quotient and Remainder.
+
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 55, num2 = 3;
+        int quotient = num1 / num2;
+        int remainder = num1 % num2;
+        System.out.println("Quotient is: " + quotient);
+        System.out.println("Remainder is: " + remainder);
+    }
+}
