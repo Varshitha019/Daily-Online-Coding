d8102dd8c4292e750a33c4e0f7240b81ff476472
Varshitha <50805446+Varshitha019@users.noreply.github.com>
2020-07-15 15:18:10 +0530

    Create Program1.py

---
+++ 14-07-2020/Program1.py
@@ -0,0 +1,12 @@
+1.Write a Python Program for Tower of Hanoi.
+
+
+def TowerOfHanoi(n , source, destination, auxilliary):
+    if n==1:
+        print("Move disk 1 from source",source,"to destination",destination)
+        return
+    TowerOfHanoi(n-1, source, auxilliary, destination)
+    print("Move disk",n,"from source",source,"to destination",destination)
+    TowerOfHanoi(n-1, auxilliary, destination, source)
+n = int(input('Enter no of disks:'))
+TowerOfHanoi(n,'A','B','C') 
