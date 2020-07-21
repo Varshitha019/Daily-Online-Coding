5357f83f22abd7698dd4e9520a9020be99afadea
Varshitha <50805446+Varshitha019@users.noreply.github.com>
2020-07-21 14:58:12 +0530

    Create Program1.py

---
+++ 20-07-2020/Program1.py
@@ -0,0 +1,9 @@
+1. Write a Python Program for Common Divisors of Two Numbers.
+
+a = int(input('Enter a: '))
+b = int(input('Enter b: '))
+n = 0
+for i in range(1, min(a, b)+1):
+    if a%i==b%i==0:
+        n+=1
+print(n)
