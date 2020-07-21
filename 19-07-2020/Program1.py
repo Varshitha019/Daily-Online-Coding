4e59d0c1d192c89be88e52ddce062c2de108dbd2
Varshitha <50805446+Varshitha019@users.noreply.github.com>
2020-07-21 14:48:59 +0530

    Create Program1.py

---
+++ 19-07-2020/Program1.py
@@ -0,0 +1,7 @@
+1.  Python Program for Number of elements with odd factors in given range.
+
+
+def countOddSquares(n, m): 
+	return int(m**0.5) - int((n-1)**0.5) 
+m = 100
+print("Count is", countOddSquares(n, m)) 
