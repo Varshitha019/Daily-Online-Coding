e89a3ac65f5b740e85ccd34df86a5e8fa724d7ae
Varshitha <50805446+Varshitha019@users.noreply.github.com>
2020-07-25 12:59:12 +0530

    Create Program1.py

---
+++ 21-07-2020/Program1.py
@@ -0,0 +1,10 @@
+1.Write a python program Basic Euclidean algorithms.
+
+
+def gcd(a, b):
+   if a == 0 :
+      return b
+   return gcd(b%a, a)
+a = int(input('Enter a:'))
+b = int(input('Enter b:'))
+print("gcd of ", a , "&" , b, " is = ", gcd(a, b))
