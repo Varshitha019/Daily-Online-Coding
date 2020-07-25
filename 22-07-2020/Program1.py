970937fbb19f57101f9d9d1b23fe716318f7453b
Varshitha <50805446+Varshitha019@users.noreply.github.com>
2020-07-25 13:10:58 +0530

    Create Program1.py

---
+++ 22-07-2020/Program1.py
@@ -0,0 +1,14 @@
+1.Write a Python Program for GCD of more than two (or array) numbers.
+
+
+def findgcd(x, y):
+   while(y):
+      x, y = y, x % y
+   return x
+l = [22, 44, 66, 88, 99]
+num1=l[0]
+num2=l[1]
+gcd=findgcd(num1,num2)
+for i in range(2,len(l)):
+   gcd=findgcd(gcd,l[i])
+print("gcd is: ",gcd)
